acd81a138d177e62fb28e61894b5e593587dd77a
Dinesh-78 <veerapareddydinesh@gmail.com>
2025-08-20 00:14:44 +0530

    build: final code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,8 +2,10 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import { globalIgnores } from "eslint/config";
 
 export default tseslint.config(
+  globalIgnores(["dist/"]),
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
